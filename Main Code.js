d6e8b5314e8ff4e55ac102170e92e0ee89b01c83
Geonwoo1109 <74478101+Geonwoo1109@users.noreply.github.com>
2021-03-19 10:59:48 +0900

    Update Main Code.js

--- Main Code.js
+++ Main Code.js
@@ -1,3 +1,5 @@
+const scriptName = "Chatlog.v2";
+
 const Fs = FileStream;
 
 var allsee = "\u200d".repeat (500);
@@ -16,19 +18,25 @@ var 시간 = ("["+hour+":"+minutes+":"+seconds+"]");
    시간 + " " + sender + "\n=> " + msg + "☆♡♧");
    
     if (msg.startsWith(".로그 ")) {
+      var abc = Number(msg.substr(4));
+      
+      if (isNaN(abc) == false) {
+      if (abc > 10000) abc = 10000;
       
        Log = Fs.read ("/sdcard/Chatlog.v2/" + room).split ("☆♡♧").reverse ();
        var b = [];
        
-        for (i = 0; i < msg.substr(4) * 1 + 1; i++) {
+        for (i = 0; i < abc; i++) {
           b.push(Log[i]);
         }
         
         replier.reply (
-        "최근 "+ msg.substr(4) +"개의 메시지"+allsee +"\n방이름: "+room +b.join("\n\n") );
+        "최근 "+ abc +"개의 메시지"+allsee +"\n방이름: "+room +b.join("\n\n") );
         b = [];
+    } else {
+      replier.reply("이상한거하지마세요");
     }
-    
+    }
     if (msg.startsWith(".로그-")) {
       
       var roomName = msg.substr(4).split("-")[0];
@@ -45,18 +53,24 @@ var 시간 = ("["+hour+":"+minutes+":"+seconds+"]");
         replier.reply (
         roomName + "방의 최근 "+ msgNumber +"개의 메시지"+allsee + c.join("\n\n") );
         c = [];
-    }
     
+    }
     if (msg == ".리셋") {
+      if (sender == "ᴳᵉᵒⁿʷᵒᵒ ᴷⁱᵐ_건우" || sender == "건우입니다") {
       Fs.remove ("/sdcard/Chatlog.v2/" + room);
       replier.reply (room + "에 저장된 채팅기록이 리셋되었습니다.");
+      
+    } else {
+      replier.reply("넌 뭐야");
     }
-    if (msg.startsWith(".리셋 ")) {
+    }
+    if (msg.startsWith(".리셋 ")/* && sender == "ᴳᵉᵒⁿʷᵒᵒ ᴷⁱᵐ_건우"*/) {
       Fs.remove ("/sdcard/Chatlog.v2/" + msg.substr(4));
       replier.reply (msg.substr(4) + "에 저장된 채팅기록이 리셋되었습니다.");
     }
     
   } catch  (e) {
+    Api.reload("Chatlog.v2");
     replier.reply (e);
   }
   
